965d8a45e6c0efccb8ce89b3e36a01a5cfa280c6
kang <kys061@gmail.com>
2014-12-08 23:11:42 +0900

    1. CLI 모드에서 실행을 위한 설정.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,25 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class MY_Controller extends CI_Controller {
-    function __construct() {
-        parent::__construct();      
-    }    
-    function _footer(){
-        $this->load->view('footer');
-    }
+	function __construct() {
+		parent::__construct();      
+		$this->load->database();
+		if(!$this->input->is_cli_request())
+			$this->load->library('session');
+		
+	}    
+	function _footer(){
+		$this->load->view('footer');
+	}
 
     function _head(){ // private method - no routing
                 //var_dump($this->session->userdata('session_test'));
                 //$this->session->set_userdata('session_test','yskang');
                 //var_dump($this->session->all_userdata());
-        $this->load->view('header');
+    	$this->load->view('header');
 
     }
     function _sidebar(){
-        $topics = $this->topic_model->gets();
-        $this->load->view('topic_list', array('topics'=>$topics));
+    	$topics = $this->topic_model->gets();
+    	$this->load->view('topic_list', array('topics'=>$topics));
     }
     function _headstory(){
-		$this->load->view('header_story');
-	}
+    	$this->load->view('header_story');
+    }
 
 }
